89013156620d49912f5aa70ece4be4d389c815cd
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-02-16 17:37:34 -0300

    requisito 2 concluido

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _instances = 0;
+    
+  constructor(name:string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._instances += 1;
+  }
+      
+  get maxLifePoints() {
+    return this._maxLifePoints;
+  }
+    
+  public static createdRacesInstances(): number {
+    return Dwarf._instances; 
+  }
+}
+
+export default Dwarf;
